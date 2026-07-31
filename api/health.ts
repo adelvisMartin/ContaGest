a06901849df84f58da3c41e653ec03188426e96c
Adelvis Camacaro <adelvismartin@gmail.com>
2026-07-31 15:16:08 -0500

    chore: expose v11.11.0 health version

--- api/health.ts
+++ api/health.ts
@@ -1,4 +1,4 @@
-const VERSION = '11.10.0';
+const VERSION = '11.11.0';
 
 export default function healthHandler(_request: unknown, response: any) {
   response.setHeader('Cache-Control', 'no-store');
